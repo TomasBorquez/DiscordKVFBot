1333a613828f6c723889425f965c0772dfb2abbf
Tomas <tomasborquez13@gmail.com>
2022-07-09 19:01:15 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const TOKEN = process.env.API_TOKEN;
+
+const client = new Discord.Client({
+  intents: [
+    'GUILDS', 
+    'GUILD_MESSAGES'
+  ],
+});
+
+client.on('ready', () => console.log(`Logged as ${client.user.tag}`))
+
+client.on("messageCreate", (message) => {
+  const messageCopy = message.content.split(" ")
+  const tag = messageCopy[0], param1 = messageCopy[1], param2 = messageCopy[2]
+  if (tag == '<@995438976200618005>') {
+    if (param1 === 'hola') message.reply('hola papito :point_right::point_left: :flushed:')
+  }
+})
+
+client.login(TOKEN)
